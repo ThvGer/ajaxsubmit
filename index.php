7b93be135d4666be193d2eccd933226171a9169e
ThvGer <thvger@outlook.fr>
2020-11-12 14:34:09 +0100

    Create Form

--- index.php
+++ index.php
@@ -7,6 +7,16 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
 <body>
+<div class="container mt-4">
+    <form id="form" action="submitform.php">
+        <div class="d-flex flex-row">
+            <input type="text" class="form-control col-2 mr-2" id="nom" aria-describedby="emailHelp">
+            <input type="text" class="form-control col-2 mr-2" id="prenom">
+            <button type="submit" class="btn btn-primary">Soumettre</button>
+        </div>
+    </form>
+</div>
+
 <?php
     include 'db.php';
 
